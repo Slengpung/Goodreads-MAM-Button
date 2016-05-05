6bda54a6cc420875e7d1debcf59fc86b98f0ffe7
Slengpung <mr.slengpung@gmail.com>
2016-05-05 13:35:15 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -5,12 +5,12 @@ var bookTitle = document.getElementById("bookTitle").innerHTML.trim().split('<',
 console.log("Book title: " + bookTitle);
 var mamSearchUrl = "https://www.myanonamouse.net/tor/browse.php?tor[text]=" + bookTitle;
 
-// Add Download@MAM button
+// Add 'Search MAM' button
 var buttonBar = document.getElementById("buyButtonContainer");
 var buttonUl  = buttonBar.getElementsByTagName("ul");
 var mamButton = document.createElement("li");
 mamButton.innerHTML = '<a id="mamLink" href="' + mamSearchUrl + '" target="_blank" class="buttonBar">Search MAM</a>';
 mamButton.className = "Button";
 buttonUl[0].appendChild(mamButton);
-console.log("Search MAM button added!");
 
+console.log("Search MAM button added!");
